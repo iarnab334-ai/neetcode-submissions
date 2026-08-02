91a6797cfbb779c91ea1d02604ff86d3a12de47b
iarnab334-ai <iarnab334@gmail.com>
2026-08-02 23:57:12 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        for (const string& s : strs) {
+            encoded += to_string(s.length()) + delimiter;
+            encoded += s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0;
+
+        while (i < s.length()) {
+            size_t delimiterPos = s.find(delimiter, i);
+            int length = stoi(s.substr(i, delimiterPos - i));
+            i = delimiterPos + 1;          // <-- fixed
+            string str = s.substr(i, length);
+            decoded.push_back(str);        // <-- fixed
+
+            i += length;
+        }
+        return decoded;
+    }
+
+private:
+    const char delimiter = '/';
+};
